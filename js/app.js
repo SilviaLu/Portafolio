ecee95dc956e03575f7721e98939d513238d86d7
SilvaLu <spvv2706@gmail.com>
2018-02-15 04:03:52 -0500

    rediseñando, photo, segunda seccion

--- js/app.js
+++ js/app.js
@@ -6,3 +6,16 @@
 //     $('.loader').fadeOut(500);
 //   }, 3000);
 // });
+
+$(document).ready(function () {
+  var altura = $('.menu').offset().top;
+  // alert(altura);
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > altura) {
+      $('.menu').addClass('menu-fixed');
+
+    } else {
+      $('.menu').removeClass('menu-fixed');
+    }
+  })
+});
